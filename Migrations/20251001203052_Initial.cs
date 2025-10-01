eb0d7b400159accc43f1eb7ed3bf75afcefdc7f5
Ayoub Khloufi <2531003@etu.cchic.ca>
2025-10-01 16:42:40 -0400

    fix

---
+++ Migrations/20251001203052_Initial.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace APIFruitFinalLeVraiFinalV2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fruit",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Couleur = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Origine = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fruit", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Fruit",
+                columns: new[] { "Id", "Couleur", "Nom", "Origine" },
+                values: new object[,]
+                {
+                    { 4, "Jaune", "Banane", "Amérique du sud" },
+                    { 5, "Vert", "Raisin", "Québec" },
+                    { 6, "Rouge", "Fraise", "Californie" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fruit");
+        }
+    }
+}
